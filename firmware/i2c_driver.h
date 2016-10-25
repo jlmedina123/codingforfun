f67140167be846d10d3f278a16927a5b7fb09b59
Jay Medina <jaymedina@apple.com>
2016-10-25 10:20:41 -0700

    firmware class

---
+++ firmware/i2c_driver.h
@@ -0,0 +1,29 @@
+#ifndef i2c_h_
+#define i2c_h_
+ 
+#define MODE_400kbps 400000
+#define AA      (1 << 2)
+#define SI      (1 << 3)
+#define STO     (1 << 4)
+#define STA     (1 << 5)
+#define I2EN    (1 << 6)
+#define ADDR     0x50   // 1010 000
+ 
+#include "LPC17xx.h"  
+ 
+ 
+void i2c1_init(void);
+uint32_t i2c1_send(uint8_t address, uint8_t* buffer, uint32_t length);
+uint32_t i2c1_receive(uint8_t address, uint8_t* buffer, uint32_t length);   
+   
+ 
+static volatile uint8_t* buf; // static not needed if members of class, but needed for global vbles
+ 
+static volatile uint32_t i2c1_busy;
+static volatile uint8_t slave_address;  
+static volatile uint32_t buf_len; 
+static volatile uint32_t num_transferred; 
+static unsigned int fdiv;
+static uint32_t ignore_data_nack = 1; 
+ 
+#endif
---
+++ firmware/pll0.h
@@ -0,0 +1,9 @@
+#ifndef pll0_h_
+#define pll0_h_
+ 
+#define myPLL_output_divider 1
+  
+void PLL0OneFeedSequence(void);
+void PLL0SetupSequence(void);
+ 
+#endif
