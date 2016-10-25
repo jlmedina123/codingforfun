8b2c694ea1d267fc385a771064d41b9076769d03
Jay Medina <jlmedina123@gmail.com>
2016-10-24 22:11:54 -0700

    8150

--- kernel/rtl8150_usbethernet.c
+++ kernel/rtl8150_usbethernet.c
@@ -24,6 +24,10 @@
  *   - usb_endpoint_descriptor
  */
 
+
+/*
+ * USB driver structs
+ */
 static struct usb_device_id rtl8150_table[] = {
         {USB_DEVICE(VENDOR_ID_REALTEK, PRODUCT_ID_RTL8150)},
         {}
@@ -32,14 +36,17 @@ static struct usb_device_id rtl8150_table[] = {
 static struct usb_driver rtl8150_driver = {
         .name           = driver_name,
         .probe          = rtl8150_probe,
-        .disconnect     = rtl8150_disconnect,
+        //.disconnect     = rtl8150_disconnect,
         .id_table       = rtl8150_table,
-        .suspend        = rtl8150_suspend,
-        .resume         = rtl8150_resume,
+        //.suspend        = rtl8150_suspend,
+        //.resume         = rtl8150_resume,
         .disable_hub_initiated_lpm = 1,
 };
 
-struct rtl8150 {
+/* 
+ * Driver private data
+ */
+typedef struct rtl8150 {
         unsigned long flags;
         struct usb_device *udev;
         struct tasklet_struct tl;
@@ -52,20 +59,58 @@ struct rtl8150 {
         int intr_interval;
         u8 *intr_buff;
         u8 phy;
-};
+} rtl8150_t;
 
+/*
+ * Net device operations
+ */
 static const struct net_device_ops rtl8150_netdev_ops = {
         .ndo_open               = rtl8150_open,
-        .ndo_stop               = rtl8150_close,
+        //.ndo_stop               = rtl8150_close,
         .ndo_do_ioctl           = rtl8150_ioctl,
         .ndo_start_xmit         = rtl8150_start_xmit,
-        .ndo_tx_timeout         = rtl8150_tx_timeout,
-        .ndo_set_rx_mode        = rtl8150_set_multicast,
-        .ndo_set_mac_address    = rtl8150_set_mac_address,
+        //.ndo_tx_timeout         = rtl8150_tx_timeout,
+        //.ndo_set_rx_mode        = rtl8150_set_multicast,
+        //.ndo_set_mac_address    = rtl8150_set_mac_address,
 
-        .ndo_change_mtu         = eth_change_mtu,
-        .ndo_validate_addr      = eth_validate_addr,
+        //.ndo_change_mtu         = eth_change_mtu,
+        //.ndo_validate_addr      = eth_validate_addr,
 };
+
+
+
+/*
+ * module methods
+ */
+module_usb_driver(rtl8150_driver);
+
+/* macros for module init/exit: 
+	
+#define module_usb_driver(__usb_driver) \
+        module_driver(__usb_driver, usb_register, \
+                       usb_deregister)
+#define usb_register(driver) \
+        usb_register_driver(driver, THIS_MODULE, KBUILD_MODNAME)
+
+#define module_driver(__driver, __register, __unregister, ...) \
+static int __init __driver##_init(void) \
+{ \
+        return __register(&(__driver) , ##__VA_ARGS__); \
+} \
+module_init(__driver##_init); \
+static void __exit __driver##_exit(void) \
+{ \
+        __unregister(&(__driver) , ##__VA_ARGS__); \
+} \
+module_exit(__driver##_exit);
+
+*/
+
+
+
+/*
+ * USB driver functions 
+ */
 
 static int rtl8150_probe(struct usb_interface *intf,
                          const struct usb_device_id *id)
@@ -73,106 +118,83 @@ static int rtl8150_probe(struct usb_interface *intf,
         struct usb_device *udev = interface_to_usbdev(intf);
         rtl8150_t *dev;
         struct net_device *netdev;
-
-        netdev = alloc_etherdev(sizeof(rtl8150_t));
-        if (!netdev)
-                return -ENOMEM;
-
-        dev = netdev_priv(netdev);
-
-        dev->intr_buff = kmalloc(INTBUFSIZE, GFP_KERNEL);
-        if (!dev->intr_buff) {
-                free_netdev(netdev);
-                return -ENOMEM;
-        }
-
-        tasklet_init(&dev->tl, rx_fixup, (unsigned long)dev);
-        spin_lock_init(&dev->rx_pool_lock);
-
-
-        dev->udev = udev;
-        dev->netdev = netdev;
-        netdev->netdev_ops = &rtl8150_netdev_ops;
-        netdev->watchdog_timeo = RTL8150_TX_TIMEOUT;
-        SET_ETHTOOL_OPS(netdev, &ops);
-        dev->intr_interval = 100;       /* 100ms */
-
-        if (!alloc_all_urbs(dev)) {
-                dev_err(&intf->dev, "out of memory\n");
-                goto out;
-        }
-        if (!rtl8150_reset(dev)) {
-                dev_err(&intf->dev, "couldn't reset the device\n");
-                goto out1;
-        }
-        fill_skb_pool(dev);
-        set_ethernet_addr(dev);
-
-        usb_set_intfdata(intf, dev);
-        SET_NETDEV_DEV(netdev, &intf->dev);
-        if (register_netdev(netdev) != 0) {
-                dev_err(&intf->dev, "couldn't register the device\n");
-                goto out2;
-        }
-        dev_info(&intf->dev, "%s: rtl8150 is detected\n", netdev->name);
-
-        return 0;
-
-out2:
-        usb_set_intfdata(intf, NULL);
-        free_skb_pool(dev);
-out1:
-        free_all_urbs(dev);
-out:
-        kfree(dev->intr_buff);
-        free_netdev(netdev);
-        return -EIO;
-}
-
-
-
-static int alloc_all_urbs(rtl8150_t * dev)
-{
-        dev->rx_urb = usb_alloc_urb(0, GFP_KERNEL);
-        if (!dev->rx_urb)
-                return 0;
-        dev->tx_urb = usb_alloc_urb(0, GFP_KERNEL);
-        if (!dev->tx_urb) {
-                usb_free_urb(dev->rx_urb);
-                return 0;
-        }
-        dev->intr_urb = usb_alloc_urb(0, GFP_KERNEL);
-        if (!dev->intr_urb) {
-                usb_free_urb(dev->rx_urb);
-                usb_free_urb(dev->tx_urb);
-                return 0;
-        }
-
-        return 1;
-}
-
-static void fill_skb_pool(rtl8150_t *dev)
-{
         struct sk_buff *skb;
         int i;
 
+        // 1) Alloc Ethernet device and initialize it
+        netdev = alloc_etherdev(sizeof(rtl8150_t));
+        netdev->netdev_ops = &rtl8150_netdev_ops;
+        netdev->watchdog_timeo = RTL8150_TX_TIMEOUT;
+        SET_ETHTOOL_OPS(netdev, &ops);
+
+
+	// 2) Get private data from netdev and initialize it
+        dev = netdev_priv(netdev);
+
+        dev->intr_buff = kmalloc(INTBUFSIZE, GFP_KERNEL);
+        tasklet_init(&dev->tl, rx_fixup, (unsigned long)dev);
+        spin_lock_init(&dev->rx_pool_lock);
+        dev->udev = udev;
+        dev->netdev = netdev;
+        dev->intr_interval = 100;       /* 100ms */
+
+	// 3) Allocate interrupt buffer
+	dev->intr_buff = kmalloc(INTBUFSIZE, GFP_KERNEL);
+
+	// 4) Allocate URBs
+	dev->rx_urb = usb_alloc_urb(0, GFP_KERNEL);
+        dev->tx_urb = usb_alloc_urb(0, GFP_KERNEL);
+        dev->intr_urb = usb_alloc_urb(0, GFP_KERNEL);
+        
+	// 5) Allocate sk buffers
         for (i = 0; i < RX_SKB_POOL_SIZE; i++) {
-                if (dev->rx_skb_pool[i])
-                        continue;
-                skb = dev_alloc_skb(RTL8150_MTU + 2);
-                if (!skb) {
-                        return;
-                }
+            	skb = dev_alloc_skb(RTL8150_MTU + 2);
                 skb_reserve(skb, 2);
                 dev->rx_skb_pool[i] = skb;
         }
+
+	// 6) Get Ethernet address and set it up
+        u8 node_id[6];
+        get_registers(dev, IDR, sizeof(node_id), node_id);
+        memcpy(dev->netdev->dev_addr, node_id, sizeof(node_id));
+
+	// 7) Attach private data to interface and to net device 
+        usb_set_intfdata(intf, dev);
+        SET_NETDEV_DEV(netdev, &intf->dev);
+
+	// 8) Register net device
+        register_netdev(netdev); 
+
+        return 0;
 }
+
 
 static int get_registers(rtl8150_t * dev, u16 indx, u16 size, void *data)
 {
-        return usb_control_msg(dev->udev, usb_rcvctrlpipe(dev->udev, 0),
-                               RTL8150_REQ_GET_REGS, RTL8150_REQT_READ,
-                               indx, 0, data, size, 500);
+	/* specify control IN endpoint number 0 
+            usb_rcvctrlpipe macro: (dev->devnum << 8) | (endpoint << 15)
+         */
+	unsigned int pipe = usb_rcvctrlpipe(dev->udev, 0);
+ 	/* build control URB, send it off, and wait for completion
+	   usb_control_msg()
+		struct usb_ctrlrequest *dr = kmalloc(sizeof(struct usb_ctrlrequest), GFP_NOIO);
+	        dr->bRequestType = requesttype;  
+                dr->bRequest = request;
+                dr->wValue = cpu_to_le16(value);
+                dr->wIndex = cpu_to_le16(index);
+                dr->wLength = cpu_to_le16(size);
+                ret = usb_internal_control_msg(dev, pipe, dr, data, size, timeout);
+                kfree(dr);
+ 	*/
+        return usb_control_msg(dev->udev, 	     /* struct usb_device */
+			       pipe,	             /* endpoint pipe to send msg to */
+                               RTL8150_REQ_GET_REGS, /* USB msg request */ 
+			       RTL8150_REQT_READ,    /* USB msg request type */
+                               indx,		     /* USB msg value */ 
+				0,		     /* USB msg index value */ 
+				data,		     /* data */ 
+				size,		     /* lenghts of data in bytes */ 
+				500);		     /* time to wait in msecs before timing out */
 }
 
 static int set_registers(rtl8150_t * dev, u16 indx, u16 size, void *data)
@@ -182,10 +204,11 @@ static int set_registers(rtl8150_t * dev, u16 indx, u16 size, void *data)
                                indx, 0, data, size, 500);
 }
 
+
+
 /*
  * Net device operations
  */
-
 
 static int rtl8150_open(struct net_device *netdev)
 {
